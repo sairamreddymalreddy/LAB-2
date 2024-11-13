9bb098bbf6a8975accb45aff563d334092a513b1
sairamreddymalreddy <sairamreddymalreddy.13@gmail.com>
2024-11-13 12:31:00 -0500

    commit

--- Quaternion.h
+++ Quaternion.h
@@ -6,11 +6,6 @@ struct Quaternion
     double x, y, z, w;
 };
 
-struct Matrix4x4 {
-    double m[4][4];
-};
-
-
 void fixedToQuaternion(double roll, double pitch, double yaw, Quaternion& q);
 void fixedToMatrix(double roll, double pitch, double yaw, GLfloat* rotation);
 void quaternionToMatrix(const Quaternion& q, GLfloat* rotation);
